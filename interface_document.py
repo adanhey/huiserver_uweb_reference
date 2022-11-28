733fe25809831354ee0418e59b5e084e87044ffe
adan <609998924@qq.com>
2022-11-28 19:15:34 +0800

    1128

--- interface_document.py
+++ interface_document.py
@@ -81,7 +81,6 @@ class Uweb_reference(Huiserver_interface):
 
     def product_delete(self, proid):
         url = '%s/es/product/delete/%s' % (self.host, proid)
-        data = {}
         result = self.get_request(url=url)
         return result
 
@@ -124,7 +123,6 @@ class Uweb_reference(Huiserver_interface):
             "uwebSourceName": uwebSourceName,
             "deleted": deleted,
         }
-        print(data)
         result = self.post_request(url=url, json=data)
         return result
 
@@ -154,8 +152,8 @@ class Uweb_reference(Huiserver_interface):
         result = self.post_request(url=url, files=data)
         return result
 
-    def model_delete(self,modelid):
-        url = '%s/es/productModel/delete/%s'%(self.host,modelid)
+    def model_delete(self, modelid):
+        url = '%s/es/productModel/delete/%s' % (self.host, modelid)
         result = self.get_request(url=url)
         return result
 
@@ -169,133 +167,157 @@ class Uweb_reference(Huiserver_interface):
         result = self.post_request(url=url, files=data)
         return result
 
-    def customer_update(self, cusid):
+    def get_org(self):
+        url = '%s/es/department/getList' % self.host
+        result = self.post_request(url=url, json={})
+        return result
+
+    def customer_update(self, fullName='uweb联通测试', nickName=None, labelList=None, contactName=None, phone=None,
+                        logoUrl=None, remark=None, organization=None, managename=None, empname=None, address=None,
+                        country=None, city=None, area=None, province=None):
         # 客户列表编辑
         url = '%s/es/customer/update' % self.host
-        phone = str(int(time.time()) * 100)[:11]
+        customerinfo = self.get_customer(fullName).json()
+        customerid = customerinfo['data']['records'][0]['id']
+        if nickName == None:
+            nickName = customerinfo['data']['records'][0]['nickName']
+        if contactName == None:
+            contactName = customerinfo['data']['records'][0]['contactName']
+        if phone == None:
+            phone = customerinfo['data']['records'][0]['phone']
+        if logoUrl == None:
+            logoUrl = customerinfo['data']['records'][0]['logoUrl']
+        if remark == None:
+            remark = customerinfo['data']['records'][0]['remark']
+        if organization == None:
+            organization = customerinfo['data']['records'][0]['organization']
+            organizationId = customerinfo['data']['records'][0]['organizationId']
+        else:
+            organizationId = None
+            orginfo = self.get_org().json()
+            for i in orginfo['data'][0]['children']:
+                if i['deptName'] == organization:
+                    organization = i['deptName']
+                    organizationId = i['id']
+                    break
+        if managename == None:
+            customerManager = customerinfo['data']['records'][0]['customerManager']
+            customerManagerId = customerinfo['data']['records'][0]['customerManagerId']
+            customerManagerPhone = customerinfo['data']['records'][0]['customerManagerPhone']
+        else:
+            maninfo = self.get_emp(managename).json()
+            customerManager = maninfo['data']['records'][0]['name']
+            customerManagerId = maninfo['data']['records'][0]['id']
+            customerManagerPhone = maninfo['data']['records'][0]['phone']
+        if empname == None:
+            employeeName = customerinfo['data']['records'][0]['customerManager']
+            employeeId = customerinfo['data']['records'][0]['customerManagerId']
+            employeePhone = customerinfo['data']['records'][0]['customerManagerPhone']
+        else:
+            empinfo = self.get_emp(managename).json()
+            employeeName = empinfo['data']['records'][0]['name']
+            employeeId = empinfo['data']['records'][0]['id']
+            employeePhone = empinfo['data']['records'][0]['phone']
+        if address == None:
+            address = customerinfo['data']['records'][0]['address']
+        if country == None:
+            country = customerinfo['data']['records'][0]['address']
+        if city == None:
+            city = customerinfo['data']['records'][0]['address']
+        if area == None:
+            area = customerinfo['data']['records'][0]['address']
+        if province == None:
+            province = customerinfo['data']['records'][0]['address']
+        lbl = []
+        if labelList:
+            dict_info = self.dict_list("customerLabel").json()
+            for i in labelList:
+                for j in dict_info['data']['records']:
+                    if i == j['dictKey']:
+                        labeldict = {
+                            "labelName": j['dictKey'],
+                            "labelId": j['id']
+                        }
+                        lbl.append(labeldict)
         data = {
-            "id": cusid,
-            "companyId": "70048941-e07c-4605-8447-2eea1a800573",
-            "customerCode": "0000102074",
-            "fullName": '%schangge' % nowtime,
-            "nickName": nowtime,
-            "labelList": [
-                {
-                    "id": "789954044564381696",
-                    "createTime": "2022-11-15 17:56:26",
-                    "updateTime": "2022-11-15 17:56:26",
-                    "createUser": "yangqunwu",
-                    "updateUser": "yangqunwu",
-                    "customerCode": "0000102074",
-                    "companyId": "70048941-e07c-4605-8447-2eea1a800573",
-                    "customerId": "755414014491926528",
-                    "labelName": "小型客户",
-                    "labelId": "746865615250477056"
-                },
-                {
-                    "id": "789954044564381697",
-                    "createTime": "2022-11-15 17:56:26",
-                    "updateTime": "2022-11-15 17:56:26",
-                    "createUser": "yangqunwu",
-                    "updateUser": "yangqunwu",
-                    "customerCode": "0000102074",
-                    "companyId": "70048941-e07c-4605-8447-2eea1a800573",
-                    "customerId": "755414014491926528",
-                    "labelName": "重点客户",
-                    "labelId": "746865491669504000"
-                },
-                {
-                    "id": "789954044564381698",
-                    "createTime": "2022-11-15 17:56:26",
-                    "updateTime": "2022-11-15 17:56:26",
-                    "createUser": "yangqunwu",
-                    "updateUser": "yangqunwu",
-                    "customerCode": "0000102074",
-                    "companyId": "70048941-e07c-4605-8447-2eea1a800573",
-                    "customerId": "755414014491926528",
-                    "labelName": "普通客户",
-                    "labelId": "746865521834938368"
-                },
-                {
-                    "id": "789954044564381699",
-                    "createTime": "2022-11-15 17:56:26",
-                    "updateTime": "2022-11-15 17:56:26",
-                    "createUser": "yangqunwu",
-                    "updateUser": "yangqunwu",
-                    "customerCode": "0000102074",
-                    "companyId": "70048941-e07c-4605-8447-2eea1a800573",
-                    "customerId": "755414014491926528",
-                    "labelName": "大型客户",
-                    "labelId": "746865550452674560"
-                }
-            ],
-            "contactName": "范大海",
+            "id": customerid,
+            "companyId": self.companyid,
+            "customerCode": self.customercode,
+            "fullName": fullName,
+            "nickName": nickName,
+            "labelList": lbl,
+            "contactName": contactName,
             "phone": phone,
-            "country": "中国",
-            "province": "湖南省",
-            "city": "长沙市",
-            "area": "天心区",
-            "address": "沙县",
-            "organization": "群武的二级组织",
-            "organizationId": "759149174878351360",
-            "employeeId": "1559727011062153218",
-            "employeeName": "郑俊鹏",
-            "employeePhone": "17688539166",
-            "customerManagerId": "755413422017126401",
-            "customerManager": "杨群武",
-            "customerManagerPhone": "15348612485",
-            "remark": "这是备注信息",
-            "logoUrl": "[{\"id\":\"789954029200646144\",\"name\":\"龙猫.gif\",\"url\":\"0000102074/customer/16685061608142491.gif\",\"uid\":1668506182845,\"status\":\"success\"}]",
-            "fieldExtensions": "{\"col10\":\"[{\\\"id\\\":\\\"789953936867237888\\\",\\\"url\\\":\\\"0000102074/customer/16685061608142491.gif\\\",\\\"name\\\":\\\"龙猫.gif\\\"}]\",\"col3\":\"\",\"col1\":\"11111\",\"col4\":\"21\"}"
+            "country": country,
+            "province": province,
+            "city": city,
+            "area": area,
+            "address": address,
+            "organization": organization,
+            "organizationId": organizationId,
+            "employeeId": employeeId,
+            "employeeName": employeeName,
+            "employeePhone": employeePhone,
+            "customerManagerId": customerManagerId,
+            "customerManager": customerManager,
+            "customerManagerPhone": customerManagerPhone,
+            "remark": remark,
+            "logoUrl": logoUrl,
         }
         result = self.post_request(url=url, json=data)
         return result
 
     def get_code(self, codetype):
         url = '%s/es/common/getCode/%s' % (self.host, codetype)
-        data = {}
         result = self.get_request(url=url)
         return result.json()['data']
 
-    def customer_save(self):
+    def customer_save(self, fullName, nickName, contactName, organization, labelList=None, address=None, country=None,
+                      city=None, area=None, province=None, remark=None, logoUrl=None, employeeName=None,
+                      customerManager=None):
+        if labelList is None:
+            labelList = []
         url = '%s/es/customer/save' % self.host
         phone = str(int(time.time()) * 10)[:11]
         code = self.get_code("KH")
+        organizationId = None
+        orginfo = self.get_org().json()
+        for i in orginfo['data'][0]['children']:
+            if i['deptName'] == organization:
+                organization = i['deptName']
+                organizationId = i['id']
+                break
         data = {
             "id": "",
             "companyId": "",
             "customerCode": "",
             "customerNumber": code,
-            "fullName": nowtime,
-            "nickName": nowtime,
-            "labelList": [
-                {
-                    "labelId": "746865462582005760",
-                    "labelName": "VIP客户"
-                }
-            ],
-            "contactName": nowtime,
+            "fullName": fullName,
+            "nickName": nickName,
+            "labelList": labelList,
+            "contactName": contactName,
             "phone": phone,
-            "country": "中国",
-            "province": "天津市",
-            "city": "市辖区",
-            "area": "和平区",
-            "address": nowtime,
+            "country": country,
+            "province": province,
+            "city": city,
+            "area": area,
+            "address": address,
             "industry": "",
-            "organization": "测试环境1",
-            "organizationId": "750492929296211968",
-            "employeeId": "",
-            "employeeName": "",
-            "employeePhone": "",
-            "customerManagerId": "",
-            "customerManager": "",
-            "customerManagerPhone": "",
-            "remark": "",
-            "logoUrl": "",
-            "fieldExtensions": "{\"col10\":\"\",\"col9\":\"\",\"col3\":\"\",\"col8\":\"\",\"col7\":\"\",\"col1\":\"1\",\"col5\":\"\",\"col2\":\"\",\"col4\":\"21\"}"
+            "organization": organization,
+            "organizationId": organizationId,
+            "remark": remark,
+            "logoUrl": logoUrl
         }
-        print(data)
-        print(url)
+        if customerManager:
+            maninfo = self.get_emp(customerManager).json()
+            data['customerManager'] = maninfo['data']['records'][0]['name']
+            data['customerManagerId'] = maninfo['data']['records'][0]['id']
+            data['customerManagerPhone'] = maninfo['data']['records'][0]['phone']
+        if employeeName:
+            empinfo = self.get_emp(customerManager).json()
+            data['employeeName'] = empinfo['data']['records'][0]['name']
+            data['employeeId'] = empinfo['data']['records'][0]['id']
+            data['employeePhone'] = empinfo['data']['records'][0]['phone']
         result = self.post_request(url=url, json=data)
         return result
 
@@ -310,25 +332,37 @@ class Uweb_reference(Huiserver_interface):
 
     def customer_delete(self, cstid):
         url = '%s/es/customer/delete/%s' % (self.host, cstid)
-        data = {}
         result = self.get_request(url=url)
         return result
 
-    def employee_save(self):
+    def employee_save(self, fullName, userName, password, email="", openService=1, role=None):
+        if role is None:
+            role = ["测试角色"]
         url = '%s/es/employee/saveUser' % self.host
         phone = str(int(time.time()) * 10)[:11]
         data = {
-            "id": "790629780094754816",
-            "fullName": "uwebreference",
-            "userName": nowtime.split(" ")[1],
-            "password": "abcd123456",
-            "password1": "abcd123456",
+            "id": "",
+            "fullName": fullName,
+            "userName": userName,
+            "password": password,
+            "password1": password,
             "linkPhone": phone,
-            "email": "",
-            "openService": 1,
-            "roleIds": "0b4dbb90-f579-4dca-93a0-e4a37b83167b"
+            "email": email,
+            "openService": openService,
+            "roleIds": ""
         }
+        rolelist = self.role_list().json()
+        for i in role:
+            for j in rolelist['data']:
+                if j['roleName'] == i:
+                    data['roleIds'] = '%s,%s' % (data['roleIds'], j['id'])
+                    break
         result = self.post_request(url=url, json=data)
+        return result
+
+    def delete_user(self,userid):
+        url = '%s/uweb-monitor/user/deleteUser?userId=%s'%(self.host,userid)
+        result = self.delete_request(url=url)
         return result
 
     def employee_update(self, empid):
@@ -369,39 +403,37 @@ class Uweb_reference(Huiserver_interface):
 
     def role_list(self):
         url = '%s/es/employee/getRoleListByCompanyId' % self.host
-        data = {}
         result = self.get_request(url=url)
         return result
 
     def event_config(self):
         # iot保养提醒
         url = '%s/es/eventPageConfig/getConfig' % self.host
-        data = {}
         result = self.get_request(url=url)
         return result
 
-    def warn_batch_status(self):
+    def warn_batch_status(self, eventIds, ids, eventType=2, handleStatus=2, handleRemark="123"):
         url = '%s/es/warn/batchUpdateStatus' % self.host
         data = {
             "eventIds": [
-                "6374a65bc26a9b59989dbefa"
+                eventIds
             ],
             "ids": [
-                "1042483972044554240"
+                ids
             ],
-            "eventType": 2,
-            "handleStatus": 2,
-            "handleRemark": "123"
+            "eventType": eventType,
+            "handleStatus": handleStatus,
+            "handleRemark": handleRemark
         }
         result = self.post_request(url=url, json=data)
         return result
 
-    def get_product(self, name):
+    def get_product(self, name, current=1, size=20):
         url = '%s/es/product/getPage' % self.host
         data = {
             "productName": name,
-            "current": 1,
-            "size": 20
+            "current": current,
+            "size": size
         }
         result = self.post_request(url=url, json=data)
         return result
@@ -418,35 +450,40 @@ class Uweb_reference(Huiserver_interface):
 
     def delete_model(self, modelid):
         url = '%s/es/productModel/delete/%s' % (self.host, modelid)
-        data = {}
         result = self.get_request(url=url)
         return result
 
-    def get_customer(self, name=None):
+    def get_customer(self, name=None, current=1, size=20, labelIds=None, organizationIds=None, customerManagerId="",
+                     employeeId="", contactName="", customerNumber=""):
+        if organizationIds is None:
+            organizationIds = []
+        if labelIds is None:
+            labelIds = []
         url = '%s/es/customer/list' % self.host
         data = {
-            "customerNumber": "",
+            "customerNumber": customerNumber,
             "fullName": name,
-            "contactName": "",
-            "employeeId": "",
-            "customerManagerId": "",
-            "organizationIds": [],
-            "labelIds": [],
-            "current": 1,
-            "size": 20
+            "contactName": contactName,
+            "employeeId": employeeId,
+            "customerManagerId": customerManagerId,
+            "organizationIds": organizationIds,
+            "labelIds": labelIds,
+            "current": current,
+            "size": size
         }
         result = self.post_request(url=url, json=data)
         return result
 
-    def get_emp(self, name="reference"):
+    def get_emp(self, name="reference", jobNumber="", jobKey="", current=1, size=20):
+        orgid = self.get_org().json()['data'][0]['id']
         url = '%s/es/employee/list' % self.host
         data = {
-            "jobNumber": "",
+            "jobNumber": jobNumber,
             "name": name,
-            "jobKey": "",
-            "deptId": "750492929296211968",
-            "current": 1,
-            "size": 20
+            "jobKey": jobKey,
+            "deptId": orgid,
+            "current": current,
+            "size": size
         }
         result = self.post_request(url=url, json=data)
         return result
@@ -479,8 +516,8 @@ b = Uweb_reference()
 # cus_id = b.get_customer("uweb_reference").json()['data']['records'][0]['id']
 # print(b.customer_delete(cus_id).text)
 # 用户
-# print(b.employee_save().text)
-# empid = b.get_emp().json()['data']['records'][0]['id']
+print(b.employee_save("11vv1", "c1ced5", "acd41e1v").text)
+# print(b.get_emp().text)
 # data = b.get_emp().json()['data']['records'][0]
 # print(b.employee_delete(data).text)
 # print(b.employee_update(empid).text)
--- test_case.py
+++ test_case.py
@@ -15,7 +15,7 @@ class TestClass:
 
 
     @allure.story("新增型号及删除")
-    def test_three(self):
+    def test_model_create_then_delete(self):
         result = b.model_saveorupdate("whchcaj","whchcaj",type='save')
         modelid = b.get_productmodel("whchcaj").json()['data']['records'][0]['id']
         result_delete = b.model_delete(modelid)
@@ -23,7 +23,7 @@ class TestClass:
         re_check(result_delete.json(),result_delete.status_code)
 
     @allure.story("批量更新型号")
-    def test_four(self):
+    def test_model_update_batch(self):
         modelresult = b.get_productmodel(current=5, size=5).json()
         modelidlist = []
         for i in modelresult['data']['records']:
@@ -32,15 +32,16 @@ class TestClass:
         re_check(result.json(),result.status_code)
 
     @allure.story("excel导入型号及删除型号")
-    def test_five(self):
+    def test_model_import_then_delete(self):
         # excel导入model
-        b.model_import_Excel()
+        result_import = b.model_import_Excel()
         modelid = b.get_productmodel("uweb_reference").json()['data']['records'][0]['id']
-        result = b.delete_model(modelid)
+        result = b.model_delete(modelid)
         re_check(result.json(),result.status_code)
+        re_check(result_import.json(),result_import.status_code)
 
     @allure.story("客户创建、更新删除")
-    def test_six(self):
+    def test_customer_create_update_delete(self):
         result = b.customer_save()
         print(result.text)
         cus_id = b.get_customer(nowtime).json()['data']['records'][0]['id']
@@ -51,7 +52,7 @@ class TestClass:
         re_check(delete_result.json(),delete_result.status_code)
 
     @allure.story("excel导入客户及删除")
-    def test_seven(self):
+    def test_customer_import_and_delete(self):
         result = b.customer_import_excel()
         cus_id = b.get_customer("uweb_reference").json()['data']['records'][0]['id']
         result_delete = b.customer_delete(cus_id)
@@ -59,7 +60,7 @@ class TestClass:
         re_check(result_delete.json(),result_delete.status_code)
 
     @allure.story("员工新增、更新、删除")
-    def test_eight(self):
+    def test_emp_create_update_delete(self):
         result = b.employee_save()
         empid = b.get_emp().json()['data']['records'][0]['id']
         data = b.get_emp().json()['data']['records'][0]
@@ -70,7 +71,7 @@ class TestClass:
         re_check(result_update.json(),result_update.status_code)
 
     @allure.story("事件查询、确认")
-    def test_nine(self):
+    def test_warn_list_comfirm(self):
         result = b.role_list()
         config_result = b.event_config()
         warn_result = b.warn_batch_status()
