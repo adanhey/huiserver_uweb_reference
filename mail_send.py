4b4f842c935aee867a7d95c89951d9238aa31e1b
adan <609998924@qq.com>
2022-12-01 17:04:42 +0800

    1201

--- mail_send.py
+++ mail_send.py
@@ -1,7 +1,6 @@
 import os
 import time
 import zipfile
-
 import win32com.client as win32
 
 nowdate = time.strftime("%Y%m%d")
@@ -18,18 +17,18 @@ class SendMail:
         # 发送多个收件人以分号分隔，中间用加号连接；实例：'@'+';'+'@'
         addressee = 'yanzhiyong@inovance.com' + ';' + 'fanhai@inovance.com' + ';' + 'yangqunwu@inovance.com'
         result, failednum, passnum = self.make_message()
-        self.zip_dir('reports')
-        # addressee = 'yangqunwu@inovance.com'
+        # self.zip_dir('reports')
+        # addressee = 'zhengjunpeng@inovance.com'
         self.mail.SentOnBehalfOfName = "zhengjunpeng@inovance.com"  # 发件人（邮箱或账号）
         self.mail.To = addressee  # 收件人
         # self.mail.CC = "receiver"        # 抄送人
         self.mail.Subject = "%s汇服务引用uweb接口自动化结果" % nowdate  # 邮件主题
         self.mail.BodyFormat = 2  # 2表示用Html format，可调整格式
         # HTMLBody插入图片：先把要插入的图片当做一个附件添加，然后在HtmlBody中调用这个图片
-        self.mail.Attachments.Add(r"E:\uweb_reference_check\allurereports.zip")  # 添加附件
-        os.remove("allurereports.zip")
+        # self.mail.Attachments.Add(r"E:\uweb_reference_check\allurereports.zip")  # 添加附件
+        # os.remove("allurereports.zip")
         passpercent = '{:.0%}'.format(passnum / (passnum + failednum))
-        self.mail.HtmlBody = "本次通过率: %s<br>失败用例：%s<br>google浏览器快捷方式-属性-目标变更为'xxx(之前的目标路径) --allow-file-access-from-files'，重新打开浏览器，打开压缩包中index.html即可查看报告" % (
+        self.mail.HtmlBody = "本次通过率: %s<br>失败用例：%s<br>allure报告地址：https://0012-1302444590.cos.ap-beijing.myqcloud.com/reports/index.html" % (
             passpercent, str(result))  # 邮件正文
         # self.mail.HtmlBody = '测试添加附件'
         # self.mail.Attachments.Add(r"C:\Users\Administrator\Pictures\Camera Roll\共产党宣言.png")    # 添加正常的附件
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import pytest
 from mail_send import *
+from send_report_to_cloud import *
 
 
 if __name__ == '__main__':
     pytest.main(['-vs','test_case.py','--clean-alluredir', '--alluredir','./allure-results'])
     os.system("allure generate ./allure-results -o ./reports --clean")
     # pytest.main("-v -s")
+    upload_result()
     m = SendMail()
     m.send_mail()
